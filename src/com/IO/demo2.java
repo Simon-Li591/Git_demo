a9d9f61feaf9e5f617d7578196d05d589763501f
simon <1311280323@qq.com>
2022-08-17 20:59:30 +0800

    IO OutputStream

--- src/com/IO/demo2.java
+++ src/com/IO/demo2.java
@@ -1,2 +1,32 @@
-package com.IO;public class demo2 {
+package com.IO;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class demo2 {
+
+    void writeFile() throws IOException {
+        OutputStream outputStream = new FileOutputStream("");
+        outputStream.write("hello".getBytes(StandardCharsets.UTF_8));
+        outputStream.close();
+    }
+
+    public static void main(String[] args) {
+        arrayByte();
+    }
+    static void arrayByte() {
+        byte[] data;
+        try (OutputStream outputStream = new ByteArrayOutputStream()) {
+            outputStream.write("hello, ".getBytes(StandardCharsets.UTF_8));
+            outputStream.write("world!".getBytes(StandardCharsets.UTF_8));
+            data = ((ByteArrayOutputStream) outputStream).toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(new String(data,StandardCharsets.UTF_8));
+    }
+
 }
